ea8763f2ce5cbc381fddedb4d52c92f913454083
Max Wayt <max@wayt.me>
2014-09-29 17:47:14 +0300

    Add Printf and Println like function to logger

--- logger/logger.go
+++ logger/logger.go
@@ -77,9 +77,29 @@ func (this *Logger) Debug(msg string) {
     this.log(LOG_DEBUG, "DEBUG", msg)
 }
 
+func (this *Logger) Debugf(msg string, v ...interface{}) {
+
+    this.Debug(fmt.Sprintf(msg, v...))
+}
+
+func (this *Logger) Debugln(v ...interface{}) {
+
+    this.Debug(fmt.Sprintln(v...))
+}
+
 func (this *Logger) Fatal(msg string) {
 
     this.log(LOG_FATAL, "FATAL", msg)
+}
+
+func (this *Logger) Fatalf(msg string, v ...interface{}) {
+
+    this.Fatal(fmt.Sprintf(msg, v...))
+}
+
+func (this *Logger) Fatalln(v ...interface{}) {
+
+    this.Fatal(fmt.Sprintln(v...))
 }
 
 func (this *Logger) Error(msg string) {
@@ -87,7 +107,29 @@ func (this *Logger) Error(msg string) {
     this.log(LOG_ERROR, "ERROR", msg)
 }
 
+func (this *Logger) Errorf(msg string, v ...interface{}) {
+
+    this.Error(fmt.Sprintf(msg, v...))
+}
+
+func (this *Logger) Errorln(v ...interface{}) {
+
+    this.Error(fmt.Sprintln(v...))
+}
+
 func (this *Logger) Info(msg string) {
 
     this.log(LOG_INFO, "INFO", msg)
 }
+
+func (this *Logger) Infof(msg string, v ...interface{}) {
+
+    this.Info(fmt.Sprintf(msg, v...))
+}
+
+func (this *Logger) Infoln(v ...interface{}) {
+
+    this.Info(fmt.Sprintln(v...))
+}
+
+
