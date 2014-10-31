d3a34cae3096a5e3249243916efc380ccef27ae0
Max Wayt <max@wayt.me>
2014-11-01 01:29:57 +0200

    Fix some validator regexp

--- validator/validator.go
+++ validator/validator.go
@@ -72,12 +72,12 @@ func Regexp(pattern string) ValidatorHandler {
 
 func IsEmail() ValidatorHandler {
 
-    return Regexp(`[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+`)
+    return Regexp(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+$`)
 }
 
 func IsNumber() ValidatorHandler {
 
-    return Regexp(`(-|)[0-9]+`)
+    return Regexp(`^(-|)[0-9]+$`)
 }
 
 func IsDate() ValidatorHandler {
