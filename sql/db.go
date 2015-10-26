5c03ae535c6f1c7b5f46ecf2aedbce14cea90d40
Max Wayt <max@wayt.me>
2015-10-26 16:57:35 +0100

    Add transaction tool to handle retry

--- sql/db.go
+++ sql/db.go
@@ -1,11 +1,13 @@
 package sql
 
 import (
+	"errors"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
 	"github.com/wayt/happyngine/env"
 	"github.com/wayt/happyngine/log"
+	"strings"
 	"time"
 )
 
@@ -42,4 +44,56 @@ func init() {
 			db.DB().Ping()
 		}
 	}()
+}
+
+// Run severale queries within a transactional context
+// The transaction is automaticly retried if needed
+func Transaction(queries ...func(tx *gorm.DB) error) error {
+
+	// Should never happens
+	if len(queries) == 0 {
+		return errors.New("No Transaction to be run...")
+	}
+
+	// Last error, used to return what happened if needed
+	var lastErr error = nil
+
+	// Retry transaction up to 3 times
+	for i := 0; i < 3; i++ {
+
+		if i != 0 { // Sleep for 50ms if it's not the first try
+			time.Sleep(50 * time.Millisecond)
+		}
+
+		// BEGIN
+		tx := DB.Begin()
+		rollback := true // In case of rollback
+		defer func() {
+			if rollback {
+				tx.Rollback()
+			}
+		}()
+
+		// Run each queries with the tx context
+		for _, q := range queries {
+
+			if lastErr = q(tx); lastErr != nil {
+				if strings.Contains(lastErr.Error(), "try restarting transaction") { // Generic error handling, can occure for multiple errors
+					continue
+				} else {
+					break // Another error, break and return via lastErr
+				}
+			}
+		}
+
+		// Try to commit the transaction, no retry here
+		if err := tx.Commit().Error; err != nil {
+			return err
+		}
+
+		// Tell to not rollback this one
+		rollback = false
+	}
+
+	return lastErr
 }
